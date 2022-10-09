3d20ae786a6e2f3e481b6878b6dd1b352bc05574
DAnimesh97 <111536941+DAnimesh97@users.noreply.github.com>
2022-10-09 16:45:55 +0530

    Add files via upload

---
+++ Animesh_FOPJava_GradedProject1/src/Departments/TechDepartment.java
@@ -0,0 +1,30 @@
+package Departments;
+
+import SuperDepartment.SuperDepartment;
+
+//Define a subclass TechDepartment which extends SuperDepartment
+
+public class TechDepartment extends SuperDepartment{
+	
+		// Define TechDepartment method to return department name
+		public String departmentName() {
+			return "Tech Department";
+		}
+
+		// Define TechDepartment method to return today's work
+		public String getTodaysWork() {
+			return "Complete coding of Module 1";
+		}
+
+		// Define TechDepartment method to return work deadline
+		public String getWorkDeadline() {
+			return "Complete by EOD";
+		}
+
+		// Define TechDepartment method to return tech stack information
+		public String getTechStackInformation() {
+			return "Core Java";
+		}
+
+	}
+
